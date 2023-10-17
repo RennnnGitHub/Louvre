1fb7d25bf1292e8c37f453a21cb21c45759a9b13
RennnnGitHub <126226483+RennnnGitHub@users.noreply.github.com>
2023-10-17 19:09:59 +1100

    Added LoginPage

--- OnlineFashionShopApp/LoginForm.Designer.cs
+++ OnlineFashionShopApp/LoginForm.Designer.cs
@@ -30,36 +30,197 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            panel2 = new Panel();
+            button2 = new Button();
+            linkLabel1 = new LinkLabel();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            linkLabel2 = new LinkLabel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.None;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Location = new Point(332, 41);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(156, 136);
+            pictureBox1.Size = new Size(131, 108);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(223, 169, 74);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(826, 35);
+            panel1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(223, 169, 74);
+            textBox2.ForeColor = Color.FromArgb(29, 65, 115);
+            textBox2.Location = new Point(3, 7);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 23);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "Louvre Shopping Application";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(223, 169, 74);
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(791, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(35, 35);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(linkLabel2);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(linkLabel1);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(207, 135);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(376, 336);
+            panel2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderColor = Color.FromArgb(223, 169, 74);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.FromArgb(223, 169, 74);
+            button2.Location = new Point(123, 239);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 47);
+            button2.TabIndex = 5;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.LightGray;
+            linkLabel1.Location = new Point(145, 205);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(100, 15);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forgot Password?";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(80, 179);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(229, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.FromArgb(223, 169, 74);
+            label3.Location = new Point(76, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(80, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(229, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.FromArgb(223, 169, 74);
+            label2.Location = new Point(76, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(223, 169, 74);
+            label1.Location = new Point(156, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            label1.Click += label1_Click;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.LinkColor = Color.White;
+            linkLabel2.Location = new Point(100, 289);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(187, 15);
+            linkLabel2.TabIndex = 6;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Don't have account? Sign up now!";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1191, 605);
+            BackColor = Color.FromArgb(29, 65, 115);
+            ClientSize = new Size(826, 573);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button1;
+        private TextBox textBox2;
+        private Panel panel2;
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+        private LinkLabel linkLabel2;
     }
 }
--- OnlineFashionShopApp/LoginForm.cs
+++ OnlineFashionShopApp/LoginForm.cs
@@ -11,5 +11,30 @@ namespace OnlineFashionShopApp
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
